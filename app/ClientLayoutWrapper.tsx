83fe21666d7c4bf81e3a201ed13fdc2756e0bbef
Hank1583 <jouchanghung@gmail.com>
2026-04-08 17:51:30 +0800

    20260408 Update V3

--- app/ClientLayoutWrapper.tsx
+++ app/ClientLayoutWrapper.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 import { usePathname } from "next/navigation";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
 
 export default function ClientLayoutWrapper({
   children,
